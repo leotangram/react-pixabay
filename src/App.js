9d8224fdd78c29ad12b2d1f8947ae7e535ed7bb3
Leonardo Omaña <leotangram@gmail.com>
2019-06-18 17:31:09 -0500

    Result image.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
+import ImagesList from './components/ImagesList'
 
 function App() {
   const [search, setSearch] = useState('')
+  const [images, setImages] = useState([])
+
   useEffect(() => {
     const consultApi = async () => {
       if (search === '') return
@@ -10,14 +13,16 @@ function App() {
       const key = '12813467-683621e872020913560670ca9'
       const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesPerPage}`
 
-      // CON AXIOS
+      // ---- CON AXIOS
       // const result = await axios.get(url)
       // console.log(result.data)
-      
-      // CON FECTH
+
+      // --- CON FECTH
       const response = await fetch(url)
       const result = await response.json()
       console.log(result)
+
+      setImages(resul.hits)
     }
     consultApi()
   }, [search])
@@ -27,7 +32,9 @@ function App() {
         <p className="lead text-center">Buscador de imágenes</p>
         <Search setSearch={setSearch} />
       </div>
-      <div className="row justify-content-center">...</div>
+      <div className="row justify-content-center">
+        <ImagesList images={images} />
+      </div>
     </div>
   )
 }
---
+++ src/components/Image.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Image = ({image}) => {
+  console.log(image);
+  
+  return (
+    <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div className="card"></div>
+    </div>
+  );
+}
+ 
+export default Image;
